91757866846a71f1df7e14c57d503128da6612b2
zhangbinalan <zhangbinalan@zhangbinalan-PC>
2014-05-10 10:47:27 +0800

    中文的properties直接用会有问题

--- CommonConfig/test/ConfigManagerTest.java
+++ CommonConfig/test/ConfigManagerTest.java
@@ -1,12 +1,11 @@
-import com.zhangbin.common.config.ConfigManager;
 import com.zhangbin.common.config.PropertiesConfig;
 import com.zhangbin.common.config.exception.ConfigException;
 
 public class ConfigManagerTest {
 	public static void main(String[] args) throws ConfigException {
-		ConfigManager.addConfig(
-				new PropertiesConfig(
-						"C:\\Users\\zhangbinalan\\git\\CommonConfig\\CommonConfig\\test\\1.properties"));
+		PropertiesConfig config = new PropertiesConfig(
+				"C:\\Users\\zhangbinalan\\git\\CommonConfig\\CommonConfig\\test\\1.properties");
+		System.out.println(config.getString("k"));
 		try {
 			Thread.sleep(60 * 1000L);
 		} catch (InterruptedException e) {
